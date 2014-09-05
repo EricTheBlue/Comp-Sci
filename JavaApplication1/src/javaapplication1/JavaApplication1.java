49af0e60270a0c07bb2bfb8ddf917f0734b2cc8d
Eric Winsor <rcwnsr@gmail.com>
2014-09-05 08:45:33 -0400

    Add First Application

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,16 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("   J      A    V     V   A   ");
+        System.out.println("   J     A A    V   V   A A  ");
+        System.out.println("J  J    AAAAA    V V   AAAAA ");
+        System.out.println(" JJ    A     A    V   A     A");
+        System.out.println();
+        System.out.println("a        a^2      a^3");
+        System.out.println("1        1        1  ");
+        System.out.println("2        4        8  ");
+        System.out.println("3        9        27 ");
+        System.out.println("4        16       64 ");
     }
     
 }
